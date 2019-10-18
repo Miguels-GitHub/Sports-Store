231997b4d2eff17589af6d0c2f5960f02a14ce3c
Miguel <miguel.barrera3@outlook.com>
2019-10-18 10:54:53 -0400

    Initial commit

---
+++ SportsStore/Models/ViewModels/CartIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using SportsStore.Models;
+
+namespace SportsStore.Models.ViewModels {
+
+    public class CartIndexViewModelew {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
